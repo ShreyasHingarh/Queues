05d7111d1a9fead1c55475e1ad69c24e35b582f4
GMR Student <gmrstudent@outlook.com>
2022-03-02 18:15:53 -0800

    idk

--- Queues/Queues/Program.cs
+++ Queues/Queues/Program.cs
@@ -6,7 +6,13 @@ namespace Queues
     {
         static void Main(string[] args)
         {
-            
+            QueueSomeone<int> queue = new QueueSomeone<int>();
+            for(int i = 0;i < 31;i++)
+            {
+                queue.Enqueue(i);
+                
+            }
+
         }
     }
 }
---
+++ Queues/Queues/QueueSomeone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queues
+{
+    class QueueSomeone<T>
+    {
+        public int Count { get; private set; }
+        private LinkedList<T> data = new LinkedList<T>();
+
+        public void Enqueue(T value) 
+        {
+            data.AddLast(value);
+        }
+        public T Dequeue() 
+        {
+            T value = data.First.Value;
+            data.RemoveFirst();
+            return value;
+        }
+        public T Peek() 
+        {
+            return data.First.Value;
+        }
+
+    }
+}
